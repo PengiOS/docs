ec9d517792079ce1fd008dd0ae623761c9c75a85
PengiCoder <125250316+PengiCoder@users.noreply.github.com>
2024-10-17 00:07:30 +0000

    add astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://pengios.github.io/docs',
+	base: 'PengiOS/Docs',
 	integrations: [
 		starlight({
 			title: 'My Docs',
